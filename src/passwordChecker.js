3ae1ec4120001096af6ab4173fffc35437059218
Fadi Samakie <fadi.samakie@student.vinci.be>
2025-01-17 17:07:58 +0100

    Implement passwordChecker function

---
+++ src/passwordChecker.js
@@ -0,0 +1,16 @@
+function passwordChecker(password) {
+    if (password.length < 8) return false;
+  
+    const specialCharRegex = /[!@#$%^&*(),.?":{}|<>]/;
+    if (!specialCharRegex.test(password)) return false;
+  
+    const digitRegex = /\d/;
+    if (!digitRegex.test(password)) return false;
+  
+    if (password.toLowerCase().includes('ipl')) return false;
+  
+    return true;
+  }
+  
+  module.exports = passwordChecker;
+  
